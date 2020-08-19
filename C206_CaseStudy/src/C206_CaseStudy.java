53cdc35766cae89e2418718b379e6aac1a1e6845
19039711 <19039711@DESKTOP-B543JIE>
2020-08-20 02:13:53 +0800

    COMMIT PART 2 HUAT AH

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -7,11 +7,55 @@ public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		run.doAddMembers();
-		run.doViewMembers();
+		run.start();
+		
+		int option = Helper.readInt("Selection Option > ");
+		
+		while (option != 6) {
+			if(option == 1) {
+				
+				Helper.line(30, "=");
+				System.out.println("Member Management");
+				Helper.line(30, "=");
+				System.out.println("Option 1 - Add Member Account");
+				System.out.println("Option 2 - View Member Account");
+				System.out.println("Option 3 - Delete Member Account");
+				System.out.println("Option 4 - Return");
+				
+				int select = Helper.readInt("Selection Option > ");
+						
+				if(select == 1) {
+					run.doAddMembers();
+				}
+				else if(select == 2) {
+					run.doViewMembers();
+				}
+				else if(select == 3) {
+						//Delete Function
+				}
+				
+					
+				
+					
+					
+			}
+		}
 	}
 
 	// MEMBER 1 - HIDIR
+	
+	public void start() {
+			System.out.println("===========================");
+			System.out.println("Course Management");
+			System.out.println("===========================");
+			System.out.println("Option 1 - Add Members");
+			System.out.println("Option 2 - Add Course Category");
+			System.out.println("Option 3 - Add Course");
+			System.out.println("Option 4 - Add Course Schedule");
+			System.out.println("Option 5 - Register for a course Schedule");
+			
+			
+	}
 
 	public void doAddMembers(){
 		String name = Helper.readString("Add Name : ");
